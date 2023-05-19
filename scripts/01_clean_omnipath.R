b60779eb120ea0aa7ca12d1511946a6fb2bf0e72
adugourd <aurelien.dugourd@bioquant.uni-heidelberg.de>
2023-05-19 10:22:54 +0200

    updated omnipath part of PKN

--- scripts/01_clean_omnipath.R
+++ scripts/01_clean_omnipath.R
@@ -2,16 +2,14 @@ library(OmnipathR)
 library(org.Hs.eg.db)
 library(readr)
 
-full_pkn <- as.data.frame(import_all_interactions())
-
-full_pkn <- full_pkn[-which(full_pkn$sources == "Wang"),] #Wang ressource looks quite problematic
+full_pkn <- as.data.frame(import_omnipath_interactions())
+full_pkn <- full_pkn[!is.na(full_pkn$references),]
 
 clean_PKN <- full_pkn[full_pkn$consensus_stimulation == 1 | full_pkn$consensus_inhibition == 1,]
-clean_PKN <- clean_PKN[clean_PKN$dorothea_level %in% c("A","A;B","A;C","A;D","B","B;D","C","C;D") | is.na(clean_PKN$dorothea_level),]
 
 clean_PKN$sign <- clean_PKN$consensus_stimulation - clean_PKN$consensus_inhibition
 
-clean_PKN <- clean_PKN[,c(3,4,18)]
+clean_PKN <- clean_PKN[,c(3,4,16)]
 
 clean_PKN_supp <- clean_PKN[clean_PKN$sign == 0,]
 clean_PKN_supp$sign <- -1
